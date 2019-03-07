9f79e4f5eedb6463076f64cb731b6de65eb39792
Allen <ajbest91@gmail.com>
2019-03-07 14:50:22 -0500

    Update Contact page
    Attempt to make the contact page work internally using php

---
+++ Contact-handler.php
@@ -0,0 +1,21 @@
+<?php
+	$name = $_POST['fname'];
+	$name = $_POST['lname'];
+	$visitor_email = $_POST['email'];
+	$message = $_POST['subject'];
+
+	$email_from = 'Githubwebsite@allen.com';
+	$email_subject = "New Form Submission";
+	$email_body = "User Name: $fname.\n".
+					"User Name: $lname.\n".
+						"User Email: $visitor_email.\n"
+							"User Message: $subject.\n"
+
+	$to = "ajbest91@gmail.com"
+	$headers = "From: $email_from \r\n";
+
+	$headers .= "Reply-To: $visitor_email \r\n";
+	mail($to, $email_subject, $email_body, $headers);
+
+	header("Location: Contact.html")
+?>
